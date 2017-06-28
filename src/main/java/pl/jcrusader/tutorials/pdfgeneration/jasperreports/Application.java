95600410fee995533725fe04ec2629ef6c97dab9
Bogumił Bierć <bogumil.bierc@gmail.com>
2017-06-28 18:47:52 +0200

    Initial commit

---
+++ src/main/java/pl/jcrusader/tutorials/pdfgeneration/jasperreports/Application.java
@@ -0,0 +1,11 @@
+package pl.jcrusader.tutorials.pdfgeneration.jasperreports;
+
+/**
+ * Created by bogumil on 6/28/17.
+ */
+public class Application {
+
+    public static void main(String args[]) {
+
+    }
+}
