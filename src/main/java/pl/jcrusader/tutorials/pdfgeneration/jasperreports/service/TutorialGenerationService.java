10c5a92fe2e86f9a59172c57dc46b59898ebda47
Bogumił Bierć <bogumil.bierc@gmail.com>
2017-06-28 19:56:05 +0200

    Generating PDF with flat structured data.

--- src/main/java/pl/jcrusader/tutorials/pdfgeneration/jasperreports/service/TutorialGenerationService.java
+++ src/main/java/pl/jcrusader/tutorials/pdfgeneration/jasperreports/service/TutorialGenerationService.java
@@ -3,6 +3,9 @@ package pl.jcrusader.tutorials.pdfgeneration.jasperreports.service;
 import pl.jcrusader.tutorials.pdfgeneration.jasperreports.generator.JasperGenerator;
 import pl.jcrusader.tutorials.pdfgeneration.jasperreports.model.simple.FlatStructuredClass;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 /**
  * Created by bogumil on 6/28/17.
  */
@@ -13,9 +16,24 @@ public class TutorialGenerationService {
 
     public TutorialGenerationService() {
         this.jasperGenerator = new JasperGenerator();
+        this.dataProviderService = new DataProviderService();
     }
 
     public void generatePdfWithFlatStructuredData() {
         FlatStructuredClass flatStructuredClass = dataProviderService.getFlatStructedClass();
+        byte[] pdfBytes = jasperGenerator.generatePdf("/templates/FlatStructuredTemplate.jrxml", flatStructuredClass);
+        saveToFile(pdfBytes, "FlatStructured.pdf");
+    }
+
+    private void saveToFile(byte[] bytes, String fileName) {
+        try {
+            File file = new File(fileName);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(bytes);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
